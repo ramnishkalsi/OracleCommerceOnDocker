7ceb1c8b9bc8e311c694fd302927bda892dbb602
Ramnish Kalsi <ramnish.kalsi@gmail.com>
2017-07-17 12:31:49 +0200

    bash into a running container.

---
+++ ssh-oc.sh
@@ -0,0 +1,7 @@
+# this will find the running endeca 11.3.0 instance & ssh into it.
+
+docker exec -it `docker ps -a | grep endeca | awk '{print $1}'` /bin/bash
+
+# service init-endeca-tools start 
+# service init-endeca-ps start 
+# service init-endeca-cas start 
