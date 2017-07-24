fbb1ac31fbe81f4bcdb33192c1b85c5ef334f9ec
Ramnish Kalsi <ramnish.kalsi@gmail.com>
2017-07-24 17:05:11 +0200

    Upgraded version for the saved container.
    - this is not the same as

---
+++ run-oc-with-discover.sh
@@ -0,0 +1,5 @@
+docker run -it -p 8006:8006 -p 8888:8888 -p 15000:15000 -p 15002:15002 -p 15010:15010 ramnishkalsi/endeca:11.3.2 /bin/bash
+
+# service init-endeca-tools start 
+# service init-endeca-ps start 
+# service init-endeca-cas start 
--- ssh-oc.sh
+++ ssh-oc.sh
@@ -1,6 +1,6 @@
 # this will find the running endeca 11.3.0 instance & ssh into it.
 
-docker exec -it `docker ps -a | grep endeca | awk '{print $1}'` /bin/bash
+docker exec -it `docker ps -a | grep "Up "| grep endeca | awk '{print $1}'` /bin/bash
 
 # service init-endeca-tools start 
 # service init-endeca-ps start 
