4f65f6b11d9a4d7502d7c6ca49366f24c3c16710
ramnishkalsi <ramnish.kalsi@gmail.com>
2017-08-18 12:54:54 +0000

    shells scripts

---
+++ endeca/scripts/analyze_dgraph_logs.sh
@@ -0,0 +1,2 @@
+/appl/endeca/MDEX/11.3.0/bin/reqloganalyzer --showAll --verbose /appl/endeca/apps/Discover/logs/dgraphs/DgraphA1/DgraphA1.reqlog
+
---
+++ endeca/scripts/run_eneperf.sh
@@ -0,0 +1,2 @@
+/appl/endeca/MDEX/11.3.0/bin/eneperf 127.0.0.1 15000 eneperf_input.txt 5 10
+
