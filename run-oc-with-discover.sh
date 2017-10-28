5e4b73206c58b480f15a385793bb339ed7b66ca6
Ramnish Kalsi <ramnish.kalsi@gmail.com>
2017-10-28 16:22:38 +0100

    With volume mapping.

--- run-oc-with-discover.sh
+++ run-oc-with-discover.sh
@@ -1,4 +1,4 @@
-docker run -it -p 8006:8006 -p 8888:8888 -p 15000:15000 -p 15002:15002 -p 15010:15010 ramnishkalsi/endeca:11.3.3 /bin/bash
+docker run -it -v /Users/ramnishkalsi/Documents:/home/endeca/Documents -p 8006:8006 -p 8888:8888 -p 15000:15000 -p 15002:15002 -p 15010:15010 ramnishkalsi/endeca:11.3.3 /bin/bash
 
 # service init-endeca-tools start 
 # service init-endeca-ps start 
